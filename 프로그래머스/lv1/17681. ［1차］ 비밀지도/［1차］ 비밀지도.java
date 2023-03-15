26928dc2813cb6a42cb5941511ff225b4df74b47
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-15 17:15:30 +0900

    [level 1] Title: [1차] 비밀지도, Time: 13.90 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,18 +2,35 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 class Solution {
-  public String[] solution(int n, int[] arr1, int[] arr2) {
-      String[] answer = new String[n];
-      String temp;
+    public String[] solution(int n, int[] arr1, int[] arr2) {
 
-      for(int i = 0 ; i < n ; i++){
-          temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
-          temp = temp.substring(temp.length() - n);
-          temp = temp.replaceAll("1", "#");
-          temp = temp.replaceAll("0", " ");
-          answer[i] = temp;
-      }
+        String[] answer = new String[n];
+        String[][] arr = new String[n][n];
 
-      return answer;
-  }
+        for (int i = 0; i < n; i++) {
+            String width_tmp = Integer.toString(arr1[i], 2);
+            width_tmp = String.format("%" + n + "s", width_tmp).replaceAll(" ", "0");
+
+            String height_tmp = Integer.toString(arr2[i], 2);
+            height_tmp = String.format("%" + n + "s", height_tmp).replaceAll(" ", "0");
+
+            System.out.println(width_tmp + " " + height_tmp);
+
+            for (int j = 0; j < n; j++) {
+                if (width_tmp.charAt(j) == '1' || height_tmp.charAt(j) == '1') {
+                    arr[i][j] = "#";
+                } else {
+                    arr[i][j] = " ";
+                }
+            }
+        }
+
+
+        int count = 0;
+        for (String[] outerArray : arr) {
+            answer[count++] = Arrays.stream(outerArray).collect(Collectors.joining());
+        }
+
+        return answer;
+    }
 }
