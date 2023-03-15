0555e71b79fe77e8d2be64da21e32fdb93240fe3
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-15 16:24:26 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.40 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 2.88 ms
+메모리: 80.2 MB, 시간: 1.40 ms
 
 ### 구분
 
--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,39 +2,18 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+      String[] answer = new String[n];
+      String temp;
 
-        String[] answer = new String[n];
-        String[][] arr = new String[n][n];
+      for(int i = 0 ; i < n ; i++){
+          temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+          temp = temp.substring(temp.length() - n);
+          temp = temp.replaceAll("1", "#");
+          temp = temp.replaceAll("0", " ");
+          answer[i] = temp;
+      }
 
-        for (int i = 0; i < n; i++) {
-            String tmp1 = Integer.toString(arr1[i], 2);
-            String tmp2 = Integer.toString(arr2[i], 2);
-
-            if (tmp1.length() < n) {
-                tmp1 = "0".repeat(n - tmp1.length()) + tmp1;
-            }
-            if (tmp2.length() < n) {
-                tmp2 = "0".repeat(n - tmp2.length()) + tmp2;
-            }
-
-            String[] wTmp = tmp1.split("");
-            String[] hTmp = tmp2.split("");
-
-            for (int j = 0; j < n; j++) {
-                if (hTmp[j].equals("1") || wTmp[j].equals("1")) {
-                    arr[i][j] = "#";
-                } else if (hTmp[j].equals("0") && wTmp[j].equals("0")) {
-                    arr[i][j] = " ";
-                }
-            }
-        }
-
-        int count = 0;
-        for (String[] outerArray : arr) {
-            answer[count++] = Arrays.stream(outerArray).collect(Collectors.joining());
-        }
-
-        return answer;
-    }
+      return answer;
+  }
 }
