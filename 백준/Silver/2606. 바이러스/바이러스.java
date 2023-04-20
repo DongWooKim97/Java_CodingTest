867e72cb59c566bbb973228f40ed12acd0eb647c
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-20 10:05:17 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,46 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int twin = sc.nextInt();
+    static ArrayList<Integer>[] list;
+    static boolean[] checked;
+    static int pc, line;
 
-        int[][] graph = new int[size + 1][size + 1];
-        int[] check = new int[size + 1];
-        Map<Integer, List<Integer>> map = new HashMap<>();
-
-//        그래프 형성
-        for (int i = 0; i < twin; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            graph[a][b] = graph[b][a] = 1;
-        }
-
-//        map에다가 해당하는 노드 연결
-        for (int i = 1; i <= size; i++) {
-            List<Integer> tmp = new ArrayList<>();
-            for (int j = 1; j <= size; j++) {
-                if (graph[i][j] == 1) {
-                    tmp.add(j);
-                }
-            }
-            map.put(i, tmp);
-        }
-
-        int count = dfs(map, check, 1, 0);
-        System.out.println(count);
-    }
-
-    public static int dfs(Map<Integer, List<Integer>> map, int[] check, int node, int count) {
-        check[node] = 1;
-        for (int i : map.get(node)) {
-            if (check[i] == 0) {
-                count = dfs(map, check, i, count+1);
+    static int DFS(int node, int count) {
+        checked[node] = true;
+        for (int i : list[node]) {
+            if(!checked[i]){
+                count = DFS(i, count + 1);
             }
         }
         return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        pc = Integer.parseInt(br.readLine());
+        line = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        list = new ArrayList[pc + 1];
+        checked = new boolean[pc + 1];
+
+        for (int i = 1; i <= pc; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < line; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int to = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            list[to].add(from);
+            list[from].add(to);
+        }
+
+        int start = 1;
+        int result = DFS(start, 0);
+        System.out.println(result);
     }
 }
