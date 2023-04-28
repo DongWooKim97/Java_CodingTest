c7f626e04e1ea5ea96aeb01721f2ef1b473e4099
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-29 00:35:33 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 472 ms, Memory: 39576 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -6,38 +6,25 @@ public class Main {
     static int count;
     static StringBuilder sb = new StringBuilder();
 
-    static void hanoi(int disk, int from, int goal) {
-        boolean[] tower = new boolean[4];
-        tower[from] = true;
-        tower[goal] = true;
-
-        int via = 0;
-        for (int i = 1; i < 4; i++) {
-            if (!tower[i]) {
-                via = i;
-                break;
-            }
-        }
-
+    static void hanoi(int disk, int start, int via, int end) {
         if (disk == 1) {
-            sb.append(from).append(" ").append(goal).append("\n");
+            sb.append(start).append(" ").append(end).append("\n");
             count++;
             return;
         }
 
-        hanoi(disk - 1, from, via);
-        hanoi(1, from, goal);
-        hanoi(disk - 1, via, goal);
+        hanoi(disk - 1, start, end, via);
+        hanoi(1, start, via, end);
+        hanoi(disk - 1, via, start, end);
     }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         count = 0;
         int disk = Integer.parseInt(br.readLine());
-        int from = 1;
-        int goal = 3;
-        hanoi(disk, from, goal);
+        hanoi(disk, 1, 2, 3);
 
         System.out.println(count);
         System.out.println(sb.toString());
