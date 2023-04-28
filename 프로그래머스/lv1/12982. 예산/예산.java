37d82b495a3ca14ef750291bf1bc4857e06c5c8c
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-28 16:47:07 +0900

    [level 1] Title: 예산, Time: 0.43 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int count = 0;
+        for (int i = 0; i < d.length; i++) {
+            if (budget - d[i] >= 0) {
+                budget -= d[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
