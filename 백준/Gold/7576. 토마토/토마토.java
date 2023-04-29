c5cf92b19fc73ef6b6f756a2e61d7bb3d03f656c
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-29 23:39:51 +0900

    [Gold V] Title: 토마토, Time: 668 ms, Memory: 124252 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,52 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
-
-class Point {
-    int x, y;
-
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+import java.util.StringTokenizer;
 
 public class Main {
-    private static final int[] moveDirectionX = {1, 0, -1, 0};
-    private static final int[] moveDirectionY = {0, 1, 0, -1};
-    private static final int numberDirectionMovement = 4;
-
-    static int n, m;
-    static int[][] graph, distance;
+    static int M, N;
+    static int[][] farm;
+    static int[][] visited;
     static Queue<Point> q = new LinkedList<>();
+    static int[] dirX = {0, 1, 0, -1};
+    static int[] dirY = {1, 0, -1, 0};
 
     static void BFS() {
         while (!q.isEmpty()) {
-            Point tmp = q.poll();
-            for (int i = 0; i < numberDirectionMovement; i++) {
-                int nextX = tmp.x + moveDirectionX[i];
-                int nextY = tmp.y + moveDirectionY[i];
-                if (0 <= nextX && nextX < n && 0 <= nextY && nextY < m && graph[nextX][nextY] == 0) {
-                    distance[nextX][nextY] = distance[tmp.x][tmp.y] + 1;
-                    graph[nextX][nextY] = 1;
-                    q.offer(new Point(nextX, nextY));
+            Point cur = q.poll();
+            for (int i = 0; i < dirX.length; i++) {
+                int nx = cur.x + dirX[i];
+                int ny = cur.y + dirY[i];
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M && farm[nx][ny] == 0) {
+                    visited[nx][ny] = visited[cur.x][cur.y] + 1;
+                    farm[nx][ny] = 1;
+                    q.offer(new Point(nx, ny));
                 }
+
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        m = sc.nextInt();
-        n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        farm = new int[N][M];
+        visited = new int[N][M];
 
-        graph = new int[n][m];
-        distance = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                graph[i][j] = sc.nextInt();
-                if (graph[i][j] == 1) {
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                farm[i][j] = tmp;
+                if (tmp == 1) {
                     q.offer(new Point(i, j));
                 }
             }
@@ -55,7 +51,7 @@ public class Main {
         BFS();
 
         boolean flag = true;
-        for (int[] w : graph) {
+        for (int[] w : farm) {
             for (int h : w) {
                 if (h == 0) {
                     flag = false;
@@ -66,7 +62,7 @@ public class Main {
 
         int answer = 0;
         if (flag) {
-            for (int[] w : distance) {
+            for (int[] w : visited) {
                 for (int h : w) {
                     answer = Math.max(h, answer);
                 }
@@ -75,5 +71,14 @@ public class Main {
         } else {
             System.out.println(-1);
         }
+    }
+}
+
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
