48a930d084a9f0c35f4f489a0824eb16b591bb0b
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-30 18:42:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 188 ms, Memory: 53800 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,26 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int[] w = new int[N+1];
-        int[] v = new int[N+1];
-        for (int i = 1; i <= N; i++) {
-            w[i] = sc.nextInt();
-            v[i] = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int item = Integer.parseInt(st.nextToken());
+        int bag = Integer.parseInt(st.nextToken());
+
+        int[] v = new int[item + 1];
+        int[] w = new int[item + 1];
+        int[][] dp = new int[item + 1][bag + 1];
+
+        for (int i = 1; i <= item; i++) {
+            st = new StringTokenizer(br.readLine());
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
         }
-        int[][] dp = new int[N+1][K+1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
+
+        for (int i = 1; i <= item; i++) {
+            for (int j = 1; j <= bag; j++) {
                 if (w[i] > j) {
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i - 1][j];
                 } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
                 }
             }
         }
-        System.out.println(dp[N][K]);
+        System.out.println(dp[item][bag]);
     }
 }
