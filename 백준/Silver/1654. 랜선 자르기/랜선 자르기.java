2861af8143c5e95965720a920fcd1cbaf5dd7643
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-05-02 01:53:09 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 17400 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,44 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[] lan = new int[K];
 
-		public static void main(String[] args) {
-				Scanner in = new Scanner(System.in);
+        long rt = 0;
 
-				int K = in.nextInt();
-				int N = in.nextInt();
+        for (int i = 0; i < K; i++) {
+            lan[i] = Integer.parseInt(br.readLine());
+            rt = Math.max(lan[i], rt);
+        }
 
-				int[] arr = new int[K];
+        rt++;
+        long mid = 0;
+        long lt = 0;
 
-				long max = 0;
+        while (lt < rt) {
+            mid = (rt + lt) / 2;
+            long take = 0;
 
-				for (int i = 0; i < K; i++) {
-						arr[i] = in.nextInt();
-						if (max < arr[i]) {
-								max = arr[i];
-						}
-				}
+            for (int x : lan) {
+                take += (x / mid);
+            }
 
-				max++;
-
-				long min = 0;
-				long mid = 0;
-
-				while (min < max) {
-						mid = (max + min) / 2;
-						long count = 0;
-
-						for (int i = 0; i < arr.length; i++) {
-								count += (arr[i] / mid);
-						}
-
-						if (count < N) {
-								max = mid;
-						} else {
-								min = mid + 1;
-						}
-				}
-
-				System.out.println(min - 1);
-		}
+            if (take < N) {
+                rt = mid;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        System.out.println(lt - 1);
+    }
 }
