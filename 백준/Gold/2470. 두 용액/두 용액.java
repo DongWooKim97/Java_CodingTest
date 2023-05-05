b60e3f3d32390fb550ea936191a7b4e822726364
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-05-05 23:44:16 +0900

    [Gold V] Title: 두 용액, Time: 408 ms, Memory: 29060 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		long[] info = new long[n];
+		st = new StringTokenizer(br.readLine());		
+		for(int i=0;i<n;i++) {
+			info[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(info); 
+		
+		int left = 0;
+		int right = n-1;
+		long res = Math.abs(info[left]+info[right]);
+		int now_left = left;
+		int now_right = right; 
+		while(now_left<now_right) {
+			long check = info[now_right]+info[now_left];
+			
+			if(Math.abs(check)<res) {
+				left = now_left;
+				right = now_right;
+				res = Math.abs(check);
+				if(res == 0) { 
+					break;
+				}
+			}
+			if(check>0) {
+				now_right -= 1;
+			}else {
+				now_left +=1;
+			}
+			//System.out.println(now_left + " "+now_right+" "+res);
+		}
+		System.out.println(info[left]+" "+info[right]);
+	}
+}
