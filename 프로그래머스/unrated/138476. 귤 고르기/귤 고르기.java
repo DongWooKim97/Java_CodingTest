5dee0469bec9c07aef8cc5f5ef2030f84d42184d
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-05-09 00:25:58 +0900

    [unrated] Title: 귤 고르기, Time: 133.64 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    static Map<Integer, Integer> map = new TreeMap<>();
+
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        for (int i : tangerine) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        List<Integer> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList, new customComparator());
+
+        for (Integer e : keyList) {
+            if (k <= 0) break;
+            answer++;
+            k -= map.get(e);
+        }
+
+        return answer;
+    }
+
+    static class customComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return map.get(o2).compareTo(map.get(o1));
+        }
+    }
+}
