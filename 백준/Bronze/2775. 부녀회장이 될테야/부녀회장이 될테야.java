bd21279baf7db3b76cf28470e3dd6ac6dd48aeb0
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-05-12 03:28:44 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 124 ms, Memory: 13968 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,34 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		// 아파트 생성 
-		int[][] APT = new int[15][15];
- 
-		for(int i = 0; i < 15; i++) {
-			APT[i][1] = 1;	// i층 1호
-			APT[0][i] = i;	// 0층 i호
-		}
- 
- 
-		for(int i = 1; i < 15; i ++) {	// 1층부터 14층까지
- 
-			for(int j = 2; j < 15; j++) {	// 2호부터 14호까지
-				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
-			}
-		}
-		
-		// 테스트 부분 		
-		int T = in.nextInt();
-		
-		for(int i = 0; i < T; i++) {
-			int k = in.nextInt();
-			int n = in.nextInt();
-			System.out.println(APT[k][n]);
-		}
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[][] building = new int[15][15];
+        for (int i = 0; i < 15; i++) { // 0층만들기
+            building[0][i] = i + 1;
+            building[i][0] = 1;
+        }
+
+        for (int i = 1; i < 15; i++) { // 그 뒤로 dp배열만들기
+            for (int j = 1; j < 15; j++) {
+                building[i][j] = building[i][j - 1] + building[i - 1][j];
+            }
+        }
+
+        for (int tc = 1; tc <= T; tc++) {
+            int floor = Integer.parseInt(br.readLine());
+            int room = Integer.parseInt(br.readLine());
+            System.out.println(building[floor][room - 1]);
+        }
+    }
 }
- 
