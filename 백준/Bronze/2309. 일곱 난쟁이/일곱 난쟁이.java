1ba51275d61f3d7d88ebafc81c42390edfbb05a4
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-05-11 21:24:40 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -4,48 +4,28 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    static int N = 9, T = 7;
-    static int target = 100;
-    static int[] height, pm, ch;
-    static StringBuilder sb = new StringBuilder();
-    static boolean check = false;
-
-    static void dfs(int level) {
-        if (level > T) return;
-        if (check) return;
-
-        if (level == T) {
-            int sum = Arrays.stream(pm).sum();
-            if (target == sum) {
-                for (int i : pm) {
-                    if (i != 0)
-                        sb.append(i).append("\n");
-                }
-                check = true;
-            }
-        } else {
-            for (int i = 0; i < N; i++) {
-                if (ch[i] == 0) {
-                    ch[i] = 1;
-                    pm[level] = height[i];
-                    dfs(level + 1);
-                    ch[i] = 0;
-                }
-            }
-        }
-    }
+    static int people = 9;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        height = new int[N];
-        ch = new int[N];
-        pm = new int[N];
-        for (int i = 0; i < N; i++) {
-            height[i] = Integer.parseInt(br.readLine());
+        int[] arr = new int[people];
+        int sum = 0;
+        for (int i = 0; i < people; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
-        Arrays.sort(height);
+        for (int i = 0; i < people - 1; i++) {
+            for (int j = i + 1; j < people; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    arr[i] = arr[j] = 0;
+                    Arrays.sort(arr);
 
-        dfs(0);
-        System.out.println(sb);
+                    for (int k = 2; k < people; k++) {
+                        System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+        }
     }
 }
