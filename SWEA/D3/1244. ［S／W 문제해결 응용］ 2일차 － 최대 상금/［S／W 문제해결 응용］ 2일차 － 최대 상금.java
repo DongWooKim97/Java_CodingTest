410312549c5a5e4e75bee88211eff23bda49bcb7
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-05-20 18:02:36 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 204 ms, Memory: 45,804 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,10 +1,12 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Solution {
+class Solution {
     static int result;
 
-    static void dfs(int cnt, int start, char[] numbers) {
-        if (cnt == 0) {
+    static void dfs(int count, int start, char[] numbers) {
+        if (count == 0) {
             int cur = charArrToInt(numbers);
             if (cur > result) {
                 result = cur;
@@ -12,46 +14,44 @@ public class Solution {
             return;
         }
 
-        for (int i = start; i < numbers.length - 1; ++i) {
-            for (int j = i + 1; j < numbers.length; ++j) {
-                int src = Integer.parseInt(String.valueOf(numbers[i]));
-                int trg = Integer.parseInt(String.valueOf(numbers[j]));
-
-                numbers[i] = (char) (trg + '0');
-                numbers[j] = (char) (src + '0');
-                dfs(cnt - 1, i, numbers);
-                numbers[i] = (char) (src + '0');
-                numbers[j] = (char) (trg + '0');
+        for (int i = start; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                numbers = swap(i, j, numbers);
+                dfs(count - 1, i, numbers);
+                numbers = swap(i, j, numbers);
             }
         }
+
     }
 
-    static int charArrToInt(char[] numbers) {
+    private static char[] swap(int i, int j, char[] numbers) {
+        char tmp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = tmp;
+
+        return numbers;
+    }
+
+
+    private static int charArrToInt(char[] numbers) {
         return Integer.parseInt(new String(numbers));
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int T = Integer.parseInt(br.readLine());
 
         for (int tc = 1; tc <= T; tc++) {
             String[] tmp = br.readLine().split(" ");
             int number = Integer.parseInt(tmp[0]);
-            int cnt = Integer.parseInt(tmp[1]);
+            int count = Integer.parseInt(tmp[1]);
 
             char[] numbers = Integer.toString(number).toCharArray();
             result = 0;
 
-            if (numbers.length < cnt) cnt = numbers.length;
-            dfs(cnt, 0, numbers);
-
-            bw.write("#" + tc + " " + result);
-            bw.newLine();
+            if (numbers.length < count) count = numbers.length;
+            dfs(count, 0, numbers);
+            System.out.printf("#%d %d\n", tc, result);
         }
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
